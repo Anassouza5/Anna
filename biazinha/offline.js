417521e961b83c277d2a5daf73c6d3dd7c73b287
Anassouza5 <50372598+Anassouza5@users.noreply.github.com>
2019-05-28 15:38:40 -0300

    Add files via upload

---
+++ biazinha/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1559068483,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/bixodoida-sheet0.png",
+		"images/botãodisparar-sheet0.png",
+		"images/botãodisparar-sheet1.png",
+		"images/balacontro-sheet0.png",
+		"images/setacima-sheet0.png",
+		"images/setadireita-sheet0.png",
+		"images/setaesquerda-sheet0.png",
+		"images/setabaixo-sheet0.png",
+		"images/fundo-sheet0.png",
+		"images/sprite2-sheet0.png",
+		"images/inimigos-sheet0.png",
+		"images/botãodestruidor-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
